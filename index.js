41e70646441e333833975c5657f35da45a21a40f
Raghav Khandelwal <gegerot@Raghavs-MacBook-Air.local>
2022-11-11 17:30:53 +0530

    procfile added

--- index.js
+++ index.js
@@ -4,19 +4,14 @@ const app = express();
 const data1 = require('./Routes/database');
 
 
-// const middleware = (req, res, next) => {
-//     console.log("Data Scanned");
-//     next();
-// }
+
 
 app.use("/home", data1);
 
 
-// app.get('/home', (rej, res) => {
-//     console.log(data1);
-//     res.send(data1);
-// })
+
 
 app.listen(process.env.PORT||8080, () => {
     console.log("Node data server created succfully!!"); 
 });
+
