3d552956d11757cacacdfdcfce2636810815f998
raghavkhandelwal11 <80354195+raghavkhandelwal11@users.noreply.github.com>
2022-11-11 18:34:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const data1 = require('./Routes/database');
 
 app.use("/home", data1);
 
-
+app.get('/', (req, res) => {
+    res.send("welcome to the posts DB server");
+})
 
 
 app.listen(process.env.PORT||8080, () => {
